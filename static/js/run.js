3397df5a64d44a80b23677caca8e3037a2b41f25
JackSnowdon <jacksnowdondrums@gmail.com>
2020-03-04 19:59:49 +0000

    creates right amount of cards per ship

--- static/js/run.js
+++ static/js/run.js
@@ -16,7 +16,20 @@ function showDetail(data) {
     if (data.next) {
         getDetail(data.next);
     } else {
-        console.log(ships); // name1.innerText = names;
+        console.log(ships);
+        var slist = document.getElementById("ship-list");
+        console.log(slist)
+        for (x = 0; ships.length; x++) {
+            console.log(ships[x].name, ships[x].model, ships[x].crew, ships[x].passengers, ships[x].films, ships[x].films.length);
+            var adddiv = document.createElement("div");
+            adddiv.setAttribute("class", "col-md-4 col-12");
+            var addcard = document.createElement("div");
+            addcard.setAttribute("class", "ship-card");
+            // var node = document.createTextNode(ships[x].name);
+            // addcard.appendChild(node);
+            adddiv.appendChild(addcard);
+            slist.appendChild(adddiv);
+        }
     }
 }
 
