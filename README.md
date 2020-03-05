cef171f5c6749d27420ce50e73275948839028db
JackSnowdon <jacksnowdondrums@gmail.com>
2020-03-05 14:00:54 +0000

    Sorted by crew number

---
+++ README.md
@@ -0,0 +1,8 @@
+# Genting Strikes Back
+
+## By Jack Snowdon
+
+A fact finding sheet for larger crewed starships of the Star Wars Universe! Built using HTML, CSS and Javascript, calling from the [SWAMPI](https://swapi.co/).
+
+
+
--- static/js/run.js
+++ static/js/run.js
@@ -1,30 +1,52 @@
+// Retrives all ships
+
 function getDetail(apiURL) {
     axios.get(apiURL).then(function(response) {
         showDetail(response.data);
     });
 }
 
+// Sorting Ship Details
+
 function showDetail(data) {
     for (i = 0; i < data.results.length; i++) {
         var crew = data.results[i].crew;
+
+        // Only pushes to ships if crew is above 10
+
         if (crew > 10) {
             ships.push(data.results[i])
         } else {
             // pass
         }
     }
+
+    // Pagination for swapi
+
     if (data.next) {
         getDetail(data.next);
     } else {
-        console.log(ships);
         var slist = document.getElementById("ship-list");
-        console.log(slist)
+
+        // Sorts ships by crew 
+
+        ships.sort(function(a, b) {
+            return parseFloat(a.crew) - parseFloat(b.crew);
+        });
+
+        // Creating and adding to the DOM
+
         for (x = 0; x < ships.length; x++) {
+
+            // Creates responive elements
+
             var adddiv = document.createElement("div");
             adddiv.setAttribute("class", "col-md-4 col-12");
 
             var addcard = document.createElement("div");
             addcard.setAttribute("class", "ship-card");
+
+            // Card header and added to parent element 
 
             var createname = document.createElement("h3");
             createname.setAttribute("class", "ship-name");
@@ -35,6 +57,8 @@ function showDetail(data) {
             createmodel.setAttribute("class", "ship-model");
             createmodel.innerHTML = " - " + ships[x].model + "<hr>";
             addcard.appendChild(createmodel)
+
+            // Card List
 
             var createnolist = document.createElement("ul");
             createnolist.setAttribute("class", "number-list");
@@ -51,7 +75,11 @@ function showDetail(data) {
             createnolist.appendChild(createpassengers)
 
             var createnofilms = document.createElement("li");
-            if (ships[x].films.length == "3") {
+            var shipfilms = ships[x].films
+
+            // Check for most film appearances 
+
+            if (shipfilms.length == "3") {
                 createnofilms.setAttribute("class", "ship-film-number-high");
                 createnofilms.innerHTML = "Films </br> " + ships[x].films.length + " <i class='far fa-star'></i>";
             } else {
@@ -60,15 +88,13 @@ function showDetail(data) {
             }
             createnolist.appendChild(createnofilms)
 
-            var shipfilms = ships[x].films
-
-            for (j = 0; j < shipfilms.length; j++) {
-                console.log(ships[x].name, shipfilms[j])
-            }
             adddiv.appendChild(addcard);
             slist.appendChild(adddiv);
         }
     }
 }
+
+// Creates array and calls function
+
 var ships = [];
 getDetail("https://swapi.co/api/starships");
